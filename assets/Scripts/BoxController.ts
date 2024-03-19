295d1983829688f3d4b47aaca81873997500279e
Leo Cheung <leozhang621@gmail.com>
2024-03-19 14:43:03 +0800

    feat: add box controll script

--- assets/Scripts/BoxController.ts
+++ assets/Scripts/BoxController.ts
@@ -1,16 +1,10 @@
-import { _decorator, Component, Node, Vec3, view } from 'cc';
-const { ccclass, property } = _decorator;
+import * as cc from 'cc';
+const { ccclass, property } = cc._decorator;
 
 @ccclass('BoxController')
-export class BoxController extends Component {
-  private _crrPostion = new Vec3();
+export class BoxController extends cc.Component {
   start() {}
 
   update(deltaTime: number) {
-    this.node.getPosition(this._crrPostion);
-    if (this._crrPostion.x <= -640) {
-        console.log('销毁box');
-        this.destroy();
-    }
   }
 }
